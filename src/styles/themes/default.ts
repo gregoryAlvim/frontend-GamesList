124c5498ad42ca3869c4190b36385caeb31b76e4
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-22 20:54:44 -0300

    Feat: created default theme with the colors variables

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  background: '#FAFAFA',
+  'base-card': '#F3F2F2',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-300': '#DBAC2C',
+  'yellow-500': '#C47F17',
+
+  'purple-100': '#EBE5F9',
+  'purple-300': '#8047F8',
+  'purple-500': '#4B2995',
+
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#D7D5D5',
+  'gray-500': '#8D8686',
+  'gray-600': '#574F4D',
+  'gray-700': '#403937',
+  'gray-800': '#272221',
+}
