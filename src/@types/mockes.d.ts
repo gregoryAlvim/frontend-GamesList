5fd32495ce9096d95fbd3ef672cf22fc9b32ca63
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-22 13:53:04 -0300

    Feat: created the theme styles type and mockes with the types

---
+++ src/@types/mockes.d.ts
@@ -0,0 +1,13 @@
+export type GameType = {
+  developer: string
+  freetogame_profile_url: string
+  game_url: string
+  genre: string
+  id: number
+  platform: string
+  publisher: string
+  release_date: string
+  short_description: string
+  thumbnail: string
+  title: string
+}
