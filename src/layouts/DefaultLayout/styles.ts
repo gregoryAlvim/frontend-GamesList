e597d31a72f62f2883da010179b565ebde250a77
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-22 20:49:57 -0300

    Feat: create Default Layout component and styleded his

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.main`
+  padding: 0 16rem;
+
+  @media (max-width: 1390px) {
+    padding: 0 10rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 2rem;
+  }
+`
