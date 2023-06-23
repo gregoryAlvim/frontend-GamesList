fcb09123857368a229be08ceabfe10b2945a78ce
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-23 14:43:52 -0300

    Feat: added platform info game and link to game url

--- src/pages/Home/components/GameCard/index.tsx
+++ src/pages/Home/components/GameCard/index.tsx
@@ -1,12 +1,17 @@
 import { GameType } from '../../../../@types/mockes'
+import { Link } from 'react-router-dom'
 import * as S from './styles'
 
 export function GameCard({ ...data }: GameType) {
   return (
     <S.Card>
-      <S.GameImage src={data.thumbnail} alt={data.short_description} />
+      <Link to={data.game_url} target="_blank">
+        <S.GameImage src={data.thumbnail} alt={data.short_description} />
+      </Link>
+
       <S.CardContent>
         <S.GameTitle>{data.title}</S.GameTitle>
+        <S.GamePlatform>{data.platform}</S.GamePlatform>
       </S.CardContent>
     </S.Card>
   )
--- src/pages/Home/components/GameCard/styles.ts
+++ src/pages/Home/components/GameCard/styles.ts
@@ -4,20 +4,37 @@ export const Card = styled.div`
   border-radius: 5px;
   width: 100%;
   height: 100%;
+  background: ${({ theme }) => theme.white};
+  border: 1px solid ${({ theme }) => theme['gray-400']};
 `
 export const CardContent = styled.section`
   padding: 20px;
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 `
 
 export const GameImage = styled.img`
   width: 100%;
   height: 20rem;
   border-radius: 5px 5px 0 0;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `
 
 export const GameTitle = styled.h1`
   color: ${({ theme }) => theme['yellow-500']};
+  font-family: 'Roboto';
+  font-size: 1.5rem;
+  font-weight: 700;
+`
+
+export const GamePlatform = styled.span`
+  color: ${({ theme }) => theme['yellow-500']};
+  font-family: 'Roboto';
+  font-size: 1.5rem;
+  font-weight: 700;
 `
