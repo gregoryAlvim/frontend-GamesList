fedad2946ddaf927c68b0b3cef76e8675e72776b
Grégory Alvim <gregori.jagua@gmail.com>
2023-06-23 14:40:40 -0300

    Feat: use react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { GamesContextProvider } from './contexts/GamesContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <BrowserRouter>
           <GamesContextProvider>
             <Router />
+            <ToastContainer toastClassName="foo" autoClose={3000} />
           </GamesContextProvider>
           <GlobalStyle />
         </BrowserRouter>
